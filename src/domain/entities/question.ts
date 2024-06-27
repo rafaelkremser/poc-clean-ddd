36ff6c2deb1141c922f06ffd6c135078fda8f7da
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-06-27 13:31:52 -0300

    feat: slug value object

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,20 +1,24 @@
 import { randomUUID } from 'node:crypto';
+import { Slug } from './value-objects/slug';
 
 interface QuestionProps {
     title: string;
     content: string;
+    slug: Slug;
     authorId: string;
 }
 
 export class Question {
     public title: string;
     public content: string;
+    public slug: Slug;
     public authorId: string;
     public id: string;
 
     constructor(props: QuestionProps, id?: string) {
         this.title = props.title;
         this.content = props.content;
+        this.slug = props.slug;
         this.authorId = props.authorId;
         this.id = id ?? randomUUID();
     }
