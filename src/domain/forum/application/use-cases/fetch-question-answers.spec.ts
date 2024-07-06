b906912d7dac7c1adbb6dbf3eb4851baf3222b26
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-07-06 15:43:51 -0300

    feat: fetch question answers use case

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.spec.ts
@@ -0,0 +1,48 @@
+import { FetchQuestionAnswersUseCase } from './fetch-question-answers';
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository';
+import { makeAnswer } from 'test/factories/make-answer';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: FetchQuestionAnswersUseCase;
+
+describe('Fetch Question Answers', () => {
+    beforeEach(() => {
+        inMemoryAnswersRepository = new InMemoryAnswersRepository();
+        sut = new FetchQuestionAnswersUseCase(inMemoryAnswersRepository);
+    });
+
+    it('should be able to fetch question answers', async () => {
+        await inMemoryAnswersRepository.create(
+            makeAnswer({ questionId: new UniqueEntityID('question-01') })
+        );
+        await inMemoryAnswersRepository.create(
+            makeAnswer({ questionId: new UniqueEntityID('question-01') })
+        );
+        await inMemoryAnswersRepository.create(
+            makeAnswer({ questionId: new UniqueEntityID('question-01') })
+        );
+
+        const { answers } = await sut.handle({
+            questionId: 'question-01',
+            page: 1,
+        });
+
+        expect(answers).toHaveLength(3);
+    });
+
+    it('should be able to fetch paginated recent questions', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await inMemoryAnswersRepository.create(
+                makeAnswer({ questionId: new UniqueEntityID('question-01') })
+            );
+        }
+
+        const { answers } = await sut.handle({
+            questionId: 'question-01',
+            page: 2,
+        });
+
+        expect(answers).toHaveLength(2);
+    });
+});
--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
 import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository';
 import { Answer } from '@/domain/forum/enterprise/entities/answer';
 
@@ -12,6 +13,14 @@ export class InMemoryAnswersRepository implements AnswersRepository {
         }
 
         return answer;
+    }
+
+    async findManyByQuestionId(questionId: string, params: PaginationParams) {
+        const answers = this.items
+            .filter((item) => item.questionId.toString() === questionId)
+            .slice((params.page - 1) * 20, params.page * 20);
+
+        return answers;
     }
 
     async create(answer: Answer) {
