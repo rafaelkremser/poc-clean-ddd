2029151d71dbe15e953e78582150b52cc7bc3bca
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-07-11 21:12:53 -0300

    refactor: add base class for comments entities

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -2,13 +2,13 @@ import { UniqueEntityID } from './unique-entity-id';
 
 export class Entity<Props> {
     private _id: UniqueEntityID;
-    protected props: any;
+    protected props: Props;
 
     get id() {
         return this._id;
     }
 
-    protected constructor(props: any, id?: UniqueEntityID) {
+    protected constructor(props: Props, id?: UniqueEntityID) {
         this.props = props;
         this._id = id ?? new UniqueEntityID(id);
     }
