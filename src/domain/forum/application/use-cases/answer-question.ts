d03d1d4dc70e45f00ff2a8af6f303e7dcb7ae266
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-07-15 11:34:26 -0300

    chore: implements all feature changes on use cases and entities

--- src/domain/forum/application/use-cases/answer-question.ts
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -2,11 +2,14 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Answer } from '@/domain/forum/enterprise/entities/answer';
 import { AnswersRepository } from '../repositories/answers-repository';
 import { Either, right } from '@/core/either';
+import { AnswerAttachmentList } from '../../enterprise/entities/answer-attachment-list';
+import { AnswerAttachment } from '../../enterprise/entities/answer-attachment';
 
 interface AnswerQuestionUseCaseRequest {
     instructorId: string;
     questionId: string;
     content: string;
+    attachmentsIds: string[];
 }
 
 type AnswerQuestionUseCaseResponse = Either<
@@ -23,12 +26,22 @@ export class AnswerQuestionUseCase {
         instructorId,
         questionId,
         content,
+        attachmentsIds,
     }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
         const answer = Answer.create({
             authorId: new UniqueEntityID(instructorId),
             questionId: new UniqueEntityID(questionId),
             content,
         });
+
+        const answerAttachments = attachmentsIds.map((attachmentId) => {
+            return AnswerAttachment.create({
+                attachmentId: new UniqueEntityID(attachmentId),
+                answerId: answer.id,
+            });
+        });
+
+        answer.attachments = new AnswerAttachmentList(answerAttachments);
 
         await this.answerRepository.create(answer);
 
--- src/domain/forum/enterprise/entities/answer-attachment.ts
+++ src/domain/forum/enterprise/entities/answer-attachment.ts
@@ -1,13 +1,13 @@
 import { Entity } from '@/core/entities/entity';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
-interface AnswerAttachmentProps {
+export interface AnswerAttachmentProps {
     answerId: UniqueEntityID;
     attachmentId: UniqueEntityID;
 }
 
 export class AnswerAttachment extends Entity<AnswerAttachmentProps> {
-    get topicId() {
+    get answerId() {
         return this.props.answerId;
     }
 
@@ -15,7 +15,7 @@ export class AnswerAttachment extends Entity<AnswerAttachmentProps> {
         return this.props.attachmentId;
     }
 
-    static create(props: AnswerAttachmentProps, id: UniqueEntityID) {
+    static create(props: AnswerAttachmentProps, id?: UniqueEntityID) {
         const attachment = new AnswerAttachment(props, id);
 
         return attachment;
