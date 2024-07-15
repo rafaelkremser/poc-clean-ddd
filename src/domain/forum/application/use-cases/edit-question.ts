8d284dbfee3f13fd58773793528028e8e9395d7b
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-07-14 21:45:45 -0300

    feat: implements watched list on question use cases

--- src/domain/forum/application/use-cases/edit-question.ts
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -3,12 +3,17 @@ import { Question } from '../../enterprise/entities/question';
 import { QuestionsRepository } from '../repositories/questions-repository';
 import { NotAllowedError } from './errors/not-allowed-error';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { QuestionAttachmentsRepository } from '../repositories/question-attachments';
+import { QuestionAttachmentList } from '../../enterprise/entities/question-attachment-list';
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
 interface EditQuestionUseCaseRequest {
     questionId: string;
     authorId: string;
     title: string;
     content: string;
+    attachmentsIds: string[];
 }
 
 type EditQuestionUseCaseResponse = Either<
@@ -19,13 +24,17 @@ type EditQuestionUseCaseResponse = Either<
 >;
 
 export class EditQuestionUseCase {
-    constructor(private questionRepository: QuestionsRepository) {}
+    constructor(
+        private questionRepository: QuestionsRepository,
+        private questionAttachmentRepository: QuestionAttachmentsRepository
+    ) {}
 
     async handle({
         questionId,
         authorId,
         title,
         content,
+        attachmentsIds,
     }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
         const question = await this.questionRepository.findById(questionId);
 
@@ -36,6 +45,24 @@ export class EditQuestionUseCase {
         if (authorId !== question.authorId.toString()) {
             return left(new NotAllowedError());
         }
+
+        const currentQuestionAttachments =
+            await this.questionAttachmentRepository.findManyByQuestionId(
+                questionId
+            );
+
+        const questionAttachmentList = new QuestionAttachmentList(
+            currentQuestionAttachments
+        );
+
+        const questionAttachments = attachmentsIds.map((attachmentId) => {
+            return QuestionAttachment.create({
+                attachmentId: new UniqueEntityID(attachmentId),
+                questionId: question.id,
+            });
+        });
+
+        questionAttachmentList.update(questionAttachments);
 
         question.title = title;
         question.content = content;
